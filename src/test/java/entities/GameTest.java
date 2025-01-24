7e73e7f3b356f272548e19aaacfe168d36f7ca80
dev-daviaraujo <dev.daviaraujo@gmail.com>
2025-01-24 00:54:28 -0300

    properties, Game, ORM

---
+++ src/test/java/entities/GameTest.java
@@ -0,0 +1,80 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getYear() {
+    }
+
+    @Test
+    void setYear() {
+    }
+
+    @Test
+    void getGenre() {
+    }
+
+    @Test
+    void setGenre() {
+    }
+
+    @Test
+    void getPlatform() {
+    }
+
+    @Test
+    void setPlatform() {
+    }
+
+    @Test
+    void getImgUrl() {
+    }
+
+    @Test
+    void setImgUrl() {
+    }
+
+    @Test
+    void getShortDescription() {
+    }
+
+    @Test
+    void setShortDescription() {
+    }
+
+    @Test
+    void getLongDescription() {
+    }
+
+    @Test
+    void setLongDescription() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
